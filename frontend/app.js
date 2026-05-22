18aa8bbf21a1119d5e635d66a2445d937378c2e2
dari_p <darielpereyra09@gmail.com>
2026-05-22 16:35:55 -0300

    Landing page y formularios iniciales

---
+++ frontend/app.js
@@ -0,0 +1,11 @@
+console.log("Peak iniciado");
+
+document.querySelectorAll("button")[0]
+.addEventListener("click", () => {
+    window.location.href = "login.html";
+});
+
+document.querySelectorAll("button")[1]
+.addEventListener("click", () => {
+    window.location.href = "register.html";
+});
